1557ea1d0c6e163e723e72fd6cd043e08745c705
khalilaccount <khalilbenmesseaoud@gmail.com>
2024-09-14 12:05:47 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+let dropButton = document.querySelector(".btn");
+let dropMenu = document.querySelector(".drop_down");
+
+dropButton.addEventListener("click", function() {
+    dropMenu.classList.toggle("show");
+});
+
+
